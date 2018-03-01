b2e68806128a0c20ef074eb66321f29dfd2cc81b
Pedro Lemos <fontebasso.lemos@gmail.com>
2018-03-01 15:56:04 -0300

    Candidatos/Pessoas
    *Novo
    -Centro de massa para os objetos
    -Filas de candidatos e pessoas
    -Candidatos sao os candidatos a pessoas, eles sao "promovidos" depois de um certo tempo em frames (definido no config.json)
    --OBS:O numero de frames eh o numero de frames q o candidato ESTAVA NA TELA
    -Pessoas sao candidatos que foram confirmados e estes sao contados e exibidos na tela.
    --OBS2: FALTA TESTAR O SISTEMA DE CANDIDATO/PESSOA, essa versao eh temporaria!!!
    
    #bugfix
    -Corrigi o problema dos quadrados nas posicoes erradas (eu simplesmente tirei o reescale, nao estava servindo pra nada mesmo)
    
    *Removido
    -Detector de pessoas treinado (q antes estava comentado), nao vejo necessidade dele agora e estava poluindo o codigo. Se precisar ele volta

--- detector.py
+++ detector.py
@@ -17,115 +17,140 @@ fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows=True)
 # inicializando o HOG descriptor/detector pre-treinado de pessoas
 hog = cv2.HOGDescriptor()
 pessoas = [] #Pessoas atualmente na imagem
+candidatos = [] #Candidatos a pessoas
 idBase = 0 #id da pessoa
-tempoMaximo = conf["tempoMaximoDeVida"] #tempo de duracao de uma pessoa na tela
+tempoMaximo = conf["tempoMaximoDeVida"] #tempo de duracao (em frames) de uma pessoa na tela
+tempoMinimo = conf["tempoMinimoDeVida"] #Tempo minimo (em frames) para ser considerado no contador
+nPessoasFrameAnterior = 0
 
 while (True):
-        
-        originalFrame = vs.read() #recebe o frame
-        if (originalFrame == None):
-        	break
 
-        #O tamanho eh alterado para facilitar a deteccao
-        #No final a imagem exibida eh a originalFrame
-        frame = mytools.resize(originalFrame,width=400)
+    originalFrame = vs.read() #recebe o frame
+    if (originalFrame == None):
+        break
 
-        #Calculo de escala da imagem resized para a do frame
-        razao_x = originalFrame.shape[1]/frame.shape[1]
-        razao_y = originalFrame.shape[0]/frame.shape[0]
-        #Essas razoes sao multiplicadas nas coordenadas para que os
-        #quadrados fiquem no lugar certo no frame
+    if nPessoasFrameAnterior != len(pessoas):
+        nPessoasFrameAnterior = len(pessoas)
+        print "numero de pessoas:",nPessoasFrameAnterior
 
-        #mascara do backgroundsubtractor
-        fgmask = fgbg.apply(frame)
+    #Verificando a idade dos candidatos
+    for c in candidatos:
+        c.envelhece()
+        if not c.vivo:
+            indice = candidatos.index(c)
+            candidatos.pop(indice)
 
-        #Aplicando um threshold para limitar aos movimentos mais importantes
-        ret,thresh = cv2.threshold(fgmask,conf["deltaThreshold"],255,cv2.THRESH_BINARY)
-
-        #Removo um pouco do noise criado pelo MOG2
-        kernelOp = np.ones((3,3),np.uint8)#Kernel opening
-        kernelCl = np.ones((11,11),np.uint8)#Kernel closing
-        thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernelOp)
-        thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernelCl)
-
-        #Depois de remover tudo o que poderia ser falso
-        #Dilato o que pode ser verdadeiro para facilitar a deteccao
-
-        #Dilato os brancos para serem melhor detectados
-        #thresh = cv2.dilate(thresh, None, iterations=1)
-
-        #Aplico um blur na imagem
-        #thresh = cv2.bilateralFilter(thresh,11,17,17)#rever os parametros
-
-        #Usar o contorno para achar os multiplos "borroes"
-        #e guardar em contours
-        img,contours,hie = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    #Verificando a idade das pessoas
+    for p in pessoas:
+        p.envelhece()
+        if not p.vivo:
+            indice = pessoas.index(p)
+            pessoas.pop(indice)
 
 
-        #Verificar o tamanho das areas, se for menor ignorar.
-        caixas = []
-        for c in contours:
-                # ignora os contornos menores que a area minima
-                if cv2.contourArea(c) < conf["areaMinimaDeteccao"]:
-                    continue
-                #Pega as coordenadas do contorno c
-                M = cv2.moments(c) #Pegando o centro da imagem
-                cx = int(M['m10']/M['m00'])
-                cy = int(M['m01']/M['m00'])
-                (x, y, w, h) = cv2.boundingRect(c)
-                novo = True
-                for i in pessoas:
-                    if (abs(x-i.x)<=w and abs(y-i.y)<=h):
-                        novo = False #Esta muito perto de um quadrado anterior
-                        i.atualiza(cx,cy)#Atualizo a posicao desse quadrado e saio
-                        break
-                if novo == True:#Se eh novo, entoa eu crio uma pessoa nova
-                    p = Pessoa(idBase,cx,cy,tempoMaximo)
-                    pessoas.append(p)
-                    idBase+=1
-                caixas.append([x,y,w+x,h+y])
+    #Essas razoes sao multiplicadas nas coordenadas para que os
+    #quadrados fiquem no lugar certo no frame
+
+    #mascara do backgroundsubtractor
+    fgmask = fgbg.apply(originalFrame)
+
+    #Aplicando um threshold para limitar aos movimentos mais importantes
+    ret,thresh = cv2.threshold(fgmask,conf["deltaThreshold"],255,cv2.THRESH_BINARY)
+
+    #Removo um pouco do noise criado pelo MOG2
+    kernelOp = np.ones((3,3),np.uint8)#Kernel opening
+    kernelCl = np.ones((11,11),np.uint8)#Kernel closing
+    thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernelOp)
+    thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernelCl)
+
+    #Depois de remover tudo o que poderia ser falso
+    #Dilato o que pode ser verdadeiro para facilitar a deteccao
+
+    #Dilato os brancos para serem melhor detectados
+    #thresh = cv2.dilate(thresh, None, iterations=1)
+
+    #Aplico um blur na imagem
+    #thresh = cv2.bilateralFilter(thresh,11,17,17)#rever os parametros
+
+    #Usar o contorno para achar os multiplos "borroes"
+    #e guardar em contours
+    img,contours,hie = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
 
 
-        #Transformo em um array do numpy para funcionar na non_max_supression
-        caixas = np.array(caixas)
+    #Verificar o tamanho das areas, se for menor ignorar.
+    caixas = []
+    for c in contours:
+            # ignora os contornos menores que a area minima
+            if cv2.contourArea(c) < conf["areaMinimaDeteccao"]:
+                continue
+            #Pega as coordenadas do contorno c
+            M = cv2.moments(c) #Pegando o centro da imagem
+            cx = int(M['m10']/M['m00'])
+            cy = int(M['m01']/M['m00'])
+            (x, y, w, h) = cv2.boundingRect(c)
+            novo = True
+            existe = False #se ele pode sair dos candidatos
+            candidatoPromovido = None #O candidato que passou para a lista de pessoas
+            cv2.circle(originalFrame,(cx,cy), 5, (0,0,255), -1) #Desenha o centro de massa
 
-        #nonMaxSupression dos objetos
-        caixas = mytools.non_max_supression(caixas,0.3)
+            #Checando se a nova coordenada faz parte de algum candidato
+            for i in candidatos:
+                if (abs(x-i.x)<=w and abs(y-i.y)<=h):
+                    novo = False #Esta muito perto de um quadrado anterior
+                    i.atualiza(cx,cy)#Atualizo a posicao desse quadrado e saio
+                    if i.confirmado: #Se ele foi confirmado e esta nos candidatos, eu vejo se ele ja existe nas pessoas
+                        for p in pessoas:
+                            if (abs(x-p.x)<=w and abs(y-p.y)<=h):
+                                p.atualiza(cx,cy)#Se ele existe em pessoas eu atualizo a respectiva pessoa
+                                indice = candidatos.index(i)
+                                candidatos.pop(indice) #e deleto ele dos candidatos
+                            else:
+                                existe = True
+                                candidatoPromovido = i
+                    break
+            #Checando se a nova coordenada faz parte das pessoas
+            for p in pessoas:
+                if (abs(x-p.x)<=w and abs(y-p.y)<=h):
+                    novo = False
+                    p.atualiza(cx,cy)
 
-        #Desenhando os quadrados
-        #print "Objetos em movimento = ",len(caixas)
-        for (startX, startY, endX, endY) in caixas:
-                pXA = int(startX*razao_x)
-                pYA = int(startY*razao_y)
-                pXB = int(razao_x*endX)
-                pYB = int(razao_y*endY)
-                cv2.rectangle(originalFrame, (pXA, pYA), (pXB, pYB), (0, 0, 255), conf["espessuraDosQuadrados"])
-
-        #Deteccao opcional
-        #Detectando pessoas na imagem original(funciona a parte da imagem alterada)
-        '''
-        hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-        (rects,weights) = hog.detectMultiScale(frame, winStride=(4, 4),
-                                                                padding=(8, 8), scale=1.05)
-
-        rects = np.array([[x, y, (x + w), (y + h)] for (x, y, w, h) in rects])
-        pick = mytools.non_max_supression(rects,0.65) #Para corrigir falsas deteccoes do HOG
-
-        #Desenhando os quadrados
-        print "Supostas pessoas = ",len(pick)
-        pick = np.array([[int((x)*razao_x), int(y*razao_y), int(razao_x*(x + w)), int(razao_y*(y + h))] for (x, y, w, h) in pick])
-        for (xA, yA, xB, yB) in pick:
-                cv2.rectangle(originalFrame, (xA, yA), (xB, yB), (255, 255, 0), conf["espessuraDosQuadrados"])
-        '''
+            #Se eh novo, entao eu crio um novo candidato
+            if novo == True:
+                p = Pessoa(idBase,cx,cy,tempoMinimo,tempoMaximo)
+                candidatos.append(p)
+                idBase+=1
+            #Se ele foi confirmado E nao existia no vetor de pessoas    
+            else: 
+                if existe:
+                    indice = candidatos.index(candidatoPromovido)
+                    candidatos.pop(indice)#tiro ele dos candidatos
+                    pessoas.append(candidatoPromovido)#e promovo ele para pessoas
+                    
+            caixas.append([x,y,w+x,h+y])
 
 
-        
-        cv2.imshow('frame',originalFrame)
+    #Transformo em um array do numpy para funcionar na non_max_supression
+    caixas = np.array(caixas)
 
-        # apertar "q" sai do loop
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord("q"):
-                break
+    #nonMaxSupression dos objetos
+    caixas = mytools.non_max_supression(caixas,0.3)
+
+    #Desenhando os quadrados
+    #print "Objetos em movimento = ",len(caixas)
+    for (startX, startY, endX, endY) in caixas:
+            pXA = int(startX)
+            pYA = int(startY)
+            pXB = int(endX)
+            pYB = int(endY)
+            cv2.rectangle(originalFrame, (pXA, pYA), (pXB, pYB), (0, 0, 255), conf["espessuraDosQuadrados"])
+
+    
+    cv2.imshow('frame',originalFrame)
+
+    # apertar "q" sai do loop
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord("q"):
+            break
 
 
 cv2.destroyAllWindows()
--- lib/pessoa.py
+++ lib/pessoa.py
@@ -1,16 +1,28 @@
 class Pessoa:
 	tracks = []
-	def __init__(self,i,x,y,idadeMax):
+	def __init__(self,i,x,y,framesMin,idadeMax):
 		self.id = i #ID da pessoa
 		self.x = x
 		self.y = y
 		self.tracks = []
 		self.idade = 0
-		self.idadeMax = idadeMax
+		self.idadeMax = idadeMax #Idade ate ser deletado
+		self.framesMin = framesMin #numero de frames para ser considerado
+		self.vivo = True
+		self.confirmado = False #Se ele existiu por tempo o suficiente para ser considerado
+		self.framesCount = 0
 	def atualiza(self,x,y):
-		self.age = 0
+		self.idade = 0
+		self.framesCount+=1 #Se passar do framesMin ele passa a ser considerado
 		self.tracks.append([self.x,self.y])#Guardando as coordenadas antigas
 		self.x = x 
 		self.y = y 
+		if self.framesCount > self.framesMin:
+			self.confirmado = True #ele existe
 	def getTracks(self):
 		return self.tracks
+	def envelhece(self):
+		self.idade+=1
+		if self.idade > self.idadeMax:
+			self.vivo = False
+
