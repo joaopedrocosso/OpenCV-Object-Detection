5ad5ebae6c5d43c57073b07a64a9da0834ebfe09
Pedro Lemos <fontebasso.lemos@gmail.com>
2018-03-06 15:14:01 -0300

    Quickfix 2
    +Mudança da posição do non max supression no código, para diminuir os "candidatos a pessoa" antes da análise de pessoas
    -Retirei algumas partes desnecessárias do código

--- detector.py
+++ detector.py
@@ -16,13 +16,31 @@ fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows=True)
 
 # inicializando o HOG descriptor/detector pre-treinado de pessoas
 hog = cv2.HOGDescriptor()
+
 pessoas = [] #Pessoas atualmente na imagem
 idBase = 0 #id da pessoa
 tempoMaximo = conf["tempoMaximoDeVida"] #tempo para delecao de uma pessoa do vetor
 tempoDeConfirmacao = conf["tempoDeConfirmacao"] #Tempo minimo (em frames) para ser considerado, como pessoa, no contador
-nPessoasFrameAnterior = 0
+nPessoasFrameAnterior = 0 #numero de pessoas confirmadas no frame anterior
+tempoInicio = time.time() #Tempo de inicio do programa
 
+#Calculo do numero de pessoas
+somatorioDaAnalise = 0 #A cada vez que o numero de pessoas muda, ele recebe o tempoDePessoa*NumeroDePessoas
+contadorDeMudancas = 0 #Conta quantas vezes o contador de pessoas mudou
+
+
+print("Iniciando:")
 while (True):
+
+    #se o tempo atingiu o tempo maximo da analise do video, pausar
+    if (time.time() - tempoInicio >= conf["duracaoAnaliseSegundos"]):
+        #FAZER MEDIA DAS INFORMACOES E GUARDAR NUM JSON
+        print("Numero aproximado de pessoas em: ",conf["duracaoAnaliseSegundos"],"segundos","=",somatorioDaAnalise/contadorDeMudancas)
+        print("Dormindo...")
+        cv2.destroyAllWindows()
+        time.sleep(conf["intervaloDescansoSegundos"]) #espera, em segundos, e recomeca
+        print("Acordei")
+        tempoInicio = time.time()
 
     originalFrame = vs.read() #recebe o frame
     if (originalFrame == None):
@@ -31,16 +49,20 @@ while (True):
     #Le o numero de pessoas ativas e exibe
     nPessoasNovo = 0
     for p in pessoas:
-        if p.confirmado:
+        if p.isConfirmado():
             nPessoasNovo+=1
+
     if nPessoasNovo != nPessoasFrameAnterior:
         print "numero de pessoas:",nPessoasFrameAnterior
         nPessoasFrameAnterior = nPessoasNovo
 
+        somatorioDaAnalise+=nPessoasNovo #atualizando o somatorio
+        contadorDeMudancas+=1
+
     #Verificando/incrementando a idade as pessoas
     for p in pessoas:
         p.envelhece()
-        if not p.vivo:
+        if not p.isVivo():
             indice = pessoas.index(p)
             pessoas.pop(indice)
 
@@ -67,28 +89,7 @@ while (True):
             # ignora os contornos menores que a area minima
             if cv2.contourArea(c) < conf["areaMinimaDeteccao"]:
                 continue
-            #Pega as coordenadas do contorno c
-            M = cv2.moments(c) #Pegando o centro da imagem
-            cx = int(M['m10']/M['m00'])
-            cy = int(M['m01']/M['m00'])
-            (x, y, w, h) = cv2.boundingRect(c)
-            novo = True
-            cv2.circle(originalFrame,(cx,cy), 5, (0,0,255), -1) #Desenha o centro de massa
-
-            #Checando se a nova coordenada faz parte das pessoas
-            for p in pessoas:
-                #Se ele ja existe
-                if (abs(x-p.x)<=w and abs(y-p.y)<=h):
-                    novo = False
-                    p.atualiza(cx,cy) #tambem atualiza o numero de frames que a pessoa apareceu
-                    break
-
-            #Se eh novo, entao eu crio uma nova pessoa
-            if novo == True:
-                p = Pessoa(idBase,cx,cy,tempoDeConfirmacao,tempoMaximo)
-                pessoas.append(p)
-                idBase+=1
-                    
+            (x, y, w, h) = cv2.boundingRect(c)              
             caixas.append([x,y,w+x,h+y])
 
 
@@ -97,6 +98,30 @@ while (True):
 
     #nonMaxSupression dos objetos
     caixas = mytools.non_max_supression(caixas,0.3)
+
+    for (x,y,endX,endY) in caixas:
+        novo = True
+        cx = endX-(endX-x)/2#centro x
+        cy = endY-(endY-y)/2#centro y
+        w = endX-x#Largura
+        h = endY-y#altura
+
+        #Checando se a nova coordenada faz parte das pessoas
+        for p in pessoas:
+            cv2.circle(originalFrame,(cx,cy), 5, (0,0,255), -1) #Desenha o centro de massa
+            #Se ele ja existe
+            if (abs(x-p.x)<=w and abs(y-p.y)<=h):
+                novo = False
+                p.atualiza(cx,cy)#tambem atualiza o numero de frames que a pessoa apareceu
+                break
+
+        #Se eh novo, entao eu crio uma nova pessoa
+        if novo == True:
+            p = Pessoa(idBase,cx,cy,tempoDeConfirmacao,tempoMaximo)
+            pessoas.append(p)
+            idBase+=1
+
+
 
     #Desenhando os quadrados
     for (startX, startY, endX, endY) in caixas:
@@ -117,3 +142,5 @@ while (True):
 
 cv2.destroyAllWindows()
 vs.stop() #para o stream
+
+
--- lib/pessoa.py
+++ lib/pessoa.py
@@ -4,13 +4,14 @@ class Pessoa:
 		self.id = i #ID da pessoa
 		self.x = x
 		self.y = y
-		self.tracks = []
-		self.idade = 0
-		self.idadeMax = idadeMax #Idade ate ser deletado
-		self.framesMin = framesMin #numero de frames para ser considerado
-		self.vivo = True
+		self.tracks = [] #lista de pontos que a pessoa esteve
+		self.idade = 0 #Idade que incrementa a cada frame (nao importando se apareceu ou nao)
+		self.idadeMax = idadeMax #Idade ateh ser deletado
+		self.framesMin = framesMin #numero de frames(em que a pessoa apareceu) para ser considerado
+		self.vivo = True #se o idade passou ou nao o idadeMax
 		self.confirmado = False #Se ele existiu por tempo o suficiente para ser considerado
-		self.framesCount = 0 #Frames em que a pessoa APARECEU
+		self.framesCount = 0 #Numero de frames em que a pessoa APARECEU
+		
 	def atualiza(self,x,y):
 		self.idade = 0
 		self.framesCount+=1 #Se passar do framesMin ele passa a ser considerado
@@ -19,10 +20,15 @@ class Pessoa:
 		self.y = y 
 		if self.framesCount > self.framesMin:
 			self.confirmado = True #ele existe
+
 	def getTracks(self):
 		return self.tracks
+
 	def envelhece(self):
 		self.idade+=1
 		if self.idade > self.idadeMax:
 			self.vivo = False
-
+	def isVivo(self):
+		return self.vivo
+	def isConfirmado(self):
+		return self.confirmado
