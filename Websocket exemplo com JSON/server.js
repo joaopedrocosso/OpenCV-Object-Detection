aa085455a9614c222786b20fd3bee241e959c7f6
Pedro Lemos <fontebasso.lemos@gmail.com>
2018-05-11 14:41:54 -0300

    Websocket parte 2
    *Servidor websocket funcionando no NodeJS.
    *O cliente em javascript no HTML do servidor web requisita o JSON do servidor e o exibe na tela

--- Websocket exemplo com JSON/server.js
+++ Websocket exemplo com JSON/server.js
@@ -1,16 +1,19 @@
 const WebSocket = require('ws');
-
 var fs = require('fs');//Leitor de arquivos, usado no json
 
-const wss = new WebSocket.Server({ port: 5555 });
+var serverAdd = "10.1.23.66";
+const wss = new WebSocket.Server({ port: 5555,host:serverAdd});
+console.log("Host at: "+serverAdd);
 const min=100;
 const max=3000;
-wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws,req) {
+  const ip = req.connection.remoteAddress;
+  console.log("Conected to: "+ip);
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
-
-    var j = JSON.parse(fs.readFileSync('./data.json','utf8'));
-    ws.send(JSON.stringify(j.chave1));
+    var j = JSON.parse(fs.readFileSync('./dados.json','utf8'));
+    ws.send(JSON.stringify(j));
+    ws.close();
   });
 });
 
